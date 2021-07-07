ddd5243d2fdc6a71b0410c5892f61ae8c78afb00
vigilant-dev <miguelito_bonfim@hotmail.com>
2021-07-07 16:39:40 -0300

    Create Logo component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React, { useState } from 'react';
-import Dashboard from './components/Dashboard';
+import React, { useState } from "react";
+import Dashboard from "./components/Dashboard";
+import Title from "./components/Title";
 
-import {Container, Typography} from '@material-ui/core'
-import "./assets/index.css"
-import 'fontsource-roboto';
-import logo from './assets/gitIcon.png'; 
+import { Container } from "@material-ui/core";
+import "fontsource-roboto";
 
-export default function App () {
-
+export default function App() {
   const [usuario, setUsuario] = useState("");
 
   function getUser(user) {
     setUsuario(user);
   }
-    
+
   return (
     <Container component="article" maxWidth="sm">
-      <div className="cabecalho">
-        <img className="logo" src={logo} alt="github" />
-        <Typography className="titulo" variant="h4" component="h1">Consulta API GitHub</Typography>    
-      </div>
-      <Dashboard usuario={usuario} aoBuscar={getUser}/>
+      <Title />
+      <Dashboard usuario={usuario} aoBuscar={getUser} />
     </Container>
   );
 }
---
+++ src/components/Title/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import LogoGit from "../LogoGit";
+import "./index.css";
+
+function Title() {
+  return (
+    <div className="logo-title">
+      <LogoGit />
+      <h1 className="title">Consulta GitHub</h1>
+    </div>
+  );
+}
+
+export default Title;

5a400c422df2bbd0211818640a6a908e814b0932
miguel-bomfim <miguel.bomfim1@hotmail.com>
2021-07-09 18:35:31 -0300

    Add react-query library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Dashboard from "./components/Dashboard";
 import Title from "./components/Title";
-
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Container } from "@material-ui/core";
 import "fontsource-roboto";
 
 export default function App() {
   const [user, setUser] = useState("");
+  const queryClient = new QueryClient();
 
   function getUser(user) {
     setUser(user);
@@ -15,9 +16,11 @@ export default function App() {
   return (
     <>
       <Title />
-      <Container component="article" maxWidth="sm">
-        <Dashboard user={user} onSearch={getUser} />
-      </Container>
+      <QueryClientProvider client={queryClient}>
+        <Container component="article" maxWidth="sm">
+          <Dashboard user={user} onSearch={getUser} />
+        </Container>
+      </QueryClientProvider>
     </>
   );
 }

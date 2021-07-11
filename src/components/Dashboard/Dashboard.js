6651a4c215c87fdc150f3de6f4cdbb73c2e3f627
miguel-bomfim <miguel.bomfim1@hotmail.com>
2021-07-11 00:25:35 -0300

    Improve user experience

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,36 +2,39 @@ import React, { useState } from "react";
 import ShowUser from "../ShowUser/ShowUser";
 import SearchUser from "../SearchUser";
 
-import { Paper, Tabs, Tab } from "@material-ui/core";
+import { Tabs, Tab } from "@material-ui/core";
 
-const Dashboard = ({ onSearch, user }) => {
+const Dashboard = () => {
+  const [user, setUser] = useState();
   const [currentStep, setCurrentStep] = useState(0);
   const [disable, setDisable] = useState(true);
 
-  const nextStep = () => {
-    setCurrentStep(currentStep + 1);
-    setDisable(false);
+  const nextStep = (value, enable) => {
+    setCurrentStep(currentStep + value);
+    setDisable(enable);
+  };
+
+  const getUser = (user) => {
+    setUser(user);
   };
 
   const steps = [
-    <SearchUser next={nextStep} onSearch={onSearch} />,
-    <ShowUser user={user} />,
+    <SearchUser next={nextStep} onSearch={getUser} />,
+    <ShowUser user={user} back={nextStep} />,
   ];
 
   return (
     <>
-      <Paper>
-        <Tabs
-          value={currentStep}
-          onChange={(event, newValue) => setCurrentStep(newValue)}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
-        >
-          <Tab label="Busca" required />
-          <Tab label="Info" disabled={disable} />
-        </Tabs>
-      </Paper>
+      <Tabs
+        value={currentStep}
+        onChange={(event, newValue) => setCurrentStep(newValue)}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Busca" required />
+        <Tab label="Info" disabled={disable} />
+      </Tabs>
       {steps[currentStep]}
     </>
   );
